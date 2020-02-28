faf662f0a5d8dcc71959814daa7679b404f466c3
David Torralba Goitia <david.torralba.goitia@gmail.com>
2020-02-28 10:48:08 +0000

    :poop: First working query with Apollo client

---
+++ src/createClient.js
@@ -0,0 +1,29 @@
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const API_ENDPOINT = 'http://localhost:8000/graphql' // TODO: with or without '/'?
+
+const defaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'ignore',
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+}
+
+export default function createApolloClient() {
+  const httpLink = createHttpLink({
+    uri: API_ENDPOINT,
+    credentials: 'include'
+  });
+
+  return new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+    defaultOptions: defaultOptions,
+  })
+}
