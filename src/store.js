98d1b42b0fe40b06702524c0e4c387112cfcdeb1
David Torralba Goitia <david.torralba.goitia@gmail.com>
2020-03-01 13:31:52 +0000

    :sparkles: Enable Redux DevTools

--- src/store.js
+++ src/store.js
@@ -1,9 +1,16 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from './rootReducer';
 
-export default createStore(
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
   rootReducer,
-  applyMiddleware(thunk),
+  /* preloadedState, */
+  composeEnhancers(
+    applyMiddleware(thunk),
+  ),
 );
+
+export default store;
