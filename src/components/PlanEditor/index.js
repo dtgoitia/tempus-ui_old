21caf1891680c2e073fbbeaf8afef4a1275406a8
David Torralba Goitia <david.torralba.goitia@gmail.com>
2020-03-06 09:04:31 +0000

    :construction: WIP. Add Plan Editor scene boilerplate

---
+++ src/components/PlanEditor/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PlanEditor() {
+  return (
+    <div>
+      Hi! I am a plan editor. I will show what is inside the selected plan and help you edit it
+    </div>
+  );
+}
---
+++ src/scenes/planEditor.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import PlanEditor from '../components/PlanEditor';
+
+export default function PlanEditorScene() {
+  // TODO: add sidebar
+  return <PlanEditor />;
+}
